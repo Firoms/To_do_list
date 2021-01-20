c2c251fb7bc3c527b701e9bf0980f125d288e60b
Firoms <edisonmessi@naver.com>
2021-01-20 18:33:20 +0900

    2021/01/20

---
+++ main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Do_list(models.Model):
+    content = models.CharField(max_length=50)
+    information = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    check = models.BooleanField(default=False)
+    state = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.id}. {self.content}"
